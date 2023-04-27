41b58a12c409aa473f6293b7ee41f79aa2b8c602
Joel Eakle <joel.c.eakle@gmail.com>
2023-04-27 12:53:49 -0400

    f8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -2,24 +2,40 @@ const add = function (a, b) {
   return a + b;
 };
 
-const subtract = function(a, b) {
-	
+const subtract = function (c, d) {
+  return c - d;
 };
 
-const sum = function() {
-	
+const sum = function(sumArr) {
+	let tSum = 0;
+  for (let i = 0; i < sumArr.length; i++){
+    if(typeof sumArr[i] === 'number'){
+      tSum += sumArr[i];
+    }
+  }
+  return tSum;
 };
 
-const multiply = function() {
-
+const multiply = function(multArr) {
+  let tMult = 0;
+  for (let i = 0; i < multArr.length; i++){
+    if(typeof multArr[i] === 'number'){
+      tMult *= multArr[i];
+    }
+  }
+  return tMult;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for (let i = 2; i <= num; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
